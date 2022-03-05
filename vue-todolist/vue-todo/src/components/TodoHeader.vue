9312f959b20084f67a131f2c51f85e6372c2369f
yangddu <wndun21@naver.com>
2022-03-05 23:47:03 +0900

    Modify: TodoHeader

--- vue-todolist/vue-todo/src/components/TodoHeader.vue
+++ vue-todolist/vue-todo/src/components/TodoHeader.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    header
+    <h1>TODO it!</h1>
   </div>
 </template>
 
-<script>
-export default {};
-</script>
-
-<style scoped></style>
+<style scoped>
+h1 {
+  color: #2f3852;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
