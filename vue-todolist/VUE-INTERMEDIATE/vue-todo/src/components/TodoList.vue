e564930a2bfa3383ae6156f899bb4aa03d9b617a
jooyoung <jooyoung@pood.pet>
2022-03-27 18:51:32 +0900

    TodoList 컴포넌트 UI 스타일링

--- vue-todolist/VUE-INTERMEDIATE/vue-todo/src/components/TodoList.vue
+++ vue-todolist/VUE-INTERMEDIATE/vue-todo/src/components/TodoList.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <ul>
-      <li v-for="todoItem in todoItems" v-bind:key="todoItem">{{todoItem}}</li>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem" class="shadow">
+        {{todoItem}}
+        <span class="removeBtn" v-on:click="removeTodo">
+          <i class="fa-solid fa-trash-can"></i>
+        </span>
+      </li>
     </ul>
   </div>
 </template>
@@ -11,6 +16,11 @@ export default {
   data: function() {
     return {
       todoItems: []
+    }
+  },
+  methods: {
+    removeTodo: function() {
+      
     }
   },
   created: function() { //인스턴스가 생성되자마자 호출되는 라이프사이클 훅 
@@ -25,4 +35,43 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0;
+  margin-top: 0;
+  text-align: left;
+}
+
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: #fff;
+  border-radius: 5px;
+}
+
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+
+.checkBtnCompleted {
+  color: #b3adad;
+}
+
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+
+</style>
