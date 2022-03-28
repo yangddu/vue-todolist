42ecb4a0ca8905fa65133706b51c060deaef2fc1
jooyoung <jooyoung@pood.pet>
2022-03-28 17:47:12 +0900

    add: 트렌지션 구현

--- vue-todolist/VUE-INTERMEDIATE/vue-todo/src/components/TodoList.vue
+++ vue-todolist/VUE-INTERMEDIATE/vue-todo/src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ul>
+    <transition-group name="list" tag="ul">
       <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
         <i class="checkBtn fa-solid fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" 
           v-on:click="toggleComplete(todoItem, index)"></i>
@@ -9,7 +9,7 @@
           <i class="fa-solid fa-trash-can"></i>
         </span>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
@@ -66,4 +66,13 @@ li {
   color: #b3adad;
 }
 
+/* 리스트 아이템 트렌지션 효과 */
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+
+.list-enter, .list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>
