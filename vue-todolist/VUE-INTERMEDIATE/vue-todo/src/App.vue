a16f6ac48f7e8ebbe564090eed328d3bba2da828
jooyoung <jooyoung@pood.pet>
2022-03-29 10:37:19 +0900

    refactor

--- vue-todolist/VUE-INTERMEDIATE/vue-todo/src/App.vue
+++ vue-todolist/VUE-INTERMEDIATE/vue-todo/src/App.vue
@@ -53,10 +53,10 @@ export default {
   },
   components: {
     //컴포넌트 태그명 : 컴포넌트 내용
-    TodoHeader: TodoHeader,
-    TodoInput: TodoInput,
-    TodoList: TodoList,
-    TodoFooter: TodoFooter
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
   }
 };
 </script>
