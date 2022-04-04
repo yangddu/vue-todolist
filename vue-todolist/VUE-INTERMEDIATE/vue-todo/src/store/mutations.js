e082e9abb784662393f98533780172791f9c40f5
jooyoung <jooyoung@pood.pet>
2022-04-04 10:46:32 +0900

    add: modules

---
+++ vue-todolist/VUE-INTERMEDIATE/vue-todo/src/store/mutations.js
@@ -0,0 +1,23 @@
+const addOneItem = (state, todoItem) => {
+    console.log('received');
+    const obj = { completed: false, item: todoItem };
+    localStorage.setItem(todoItem, JSON.stringify(obj));
+    state.todoItems.push(obj);
+}
+
+const removeOneItem = (state, payload) => {
+    localStorage.removeItem(payload.todoItem.item);
+    state.todoItems.splice(payload.index, 1); 
+}
+
+const toggleOneItem = (state, payload) => {
+    state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
+    //로컬 스토리지의 데이터를 갱신
+    localStorage.removeItem(payload.todoItem.item);
+    localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
+}
+
+const clearAllItems = (state) => {
+    localStorage.clear();
+    state.todoItems = [];
+}
--- vue-todolist/VUE-INTERMEDIATE/vue-todo/src/store/store.js
+++ vue-todolist/VUE-INTERMEDIATE/vue-todo/src/store/store.js
@@ -19,7 +19,7 @@ const storage = {
 
 export const store = new Vuex.Store({
     state: {
-        todoItems: storage.fetch(), // 모든 컴포넌트에서 공통적으로 쓸 수 있는 데이터
+        // todoItems: storage.fetch(),  모든 컴포넌트에서 공통적으로 쓸 수 있는 데이터
         num: 10,
         storeNum: 10,
     },
@@ -29,26 +29,26 @@ export const store = new Vuex.Store({
         },
     },
     mutations: { //mutations에서 state로 접근하는 방법은 첫번째로 주어진 인자 state를 접근해서 안에 속성으로 접근
-        addOneItem(state, todoItem) {
-            console.log('received');
-            const obj = { completed: false, item: todoItem };
-            localStorage.setItem(todoItem, JSON.stringify(obj));
-            state.todoItems.push(obj);
-        },
-        removeOneItem(state, payload) {
-            localStorage.removeItem(payload.todoItem.item);
-            state.todoItems.splice(payload.index, 1); 
-        },
-        toggleOneItem(state, payload) {
-            state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
-            //로컬 스토리지의 데이터를 갱신
-            localStorage.removeItem(payload.todoItem.item);
-            localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
-        },
-        clearAllItems(state) {
-            localStorage.clear();
-            state.todoItems = [];
-        },
+        // addOneItem(state, todoItem) {
+        //     console.log('received');
+        //     const obj = { completed: false, item: todoItem };
+        //     localStorage.setItem(todoItem, JSON.stringify(obj));
+        //     state.todoItems.push(obj);
+        // },
+        // removeOneItem(state, payload) {
+        //     localStorage.removeItem(payload.todoItem.item);
+        //     state.todoItems.splice(payload.index, 1); 
+        // },
+        // toggleOneItem(state, payload) {
+        //     state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
+        //     //로컬 스토리지의 데이터를 갱신
+        //     localStorage.removeItem(payload.todoItem.item);
+        //     localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
+        // },
+        // clearAllItems(state) {
+        //     localStorage.clear();
+        //     state.todoItems = [];
+        // },
         
     }
 });
